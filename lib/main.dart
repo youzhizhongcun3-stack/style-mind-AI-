0e8f19de63bf330c764a94ba997e5296786ff861
Yu-sh <yushinakamura781@gmail.com>
2026-06-28 15:57:39 +0900

    Add: Rakuten affiliate ID (A8.net) to purchase links

--- lib/main.dart
+++ lib/main.dart
@@ -733,7 +733,8 @@ class _ChatScreenState extends State<ChatScreen> {
           } else if (!isLuxury) {
             // プチプラ・一般ブランドのみユニクロに送る
           }
-          shops.add({'name': 'Rakuten Fashion', 'icon': '🏪', 'url': 'https://search.rakuten.co.jp/search/mall/$encoded/100533/'});
+          final rakutenSearchUrl = Uri.encodeComponent('https://search.rakuten.co.jp/search/mall/${Uri.decodeComponent(encoded)}/100533/');
+          shops.add({'name': 'Rakuten Fashion', 'icon': '🏪', 'url': 'https://rpx.a8.net/svt/ejp?a8mat=4B66KH+C6SHMA+2HOM+BW8O1&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0ea62065.34400275.0ea62066.204f04c0%2Fa26062830024_4B66KH_C6SHMA_2HOM_BW8O1%3Fpc%3D$rakutenSearchUrl'});
           shops.add({'name': 'Amazon', 'icon': '📦', 'url': 'https://www.amazon.co.jp/s?k=$encoded&i=fashion'});
 
           items.add({
